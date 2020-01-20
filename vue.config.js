aaf6dc62ee8991c6a4d5a3c182b4a071d4a83709
mark.duo <mark.wang@shijigroup.com>
2020-01-20 11:43:06 +0800

    设置移动端和pc端不同环境

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   // 部署应用包时的基本 URL,从 Vue CLI 3.3 起已弃用baseUrl
   publicPath: '/',
-  outputDir: path.resolve(__dirname, process.env.NODE_ENV === 'development' ? 'alpha' : 'dist'), // 运行时生成的生产环境构建文件的目录(默认''dist''，构建之前会被清除)
+  outputDir: path.resolve(__dirname, process.env.NODE_ENV === 'moblie' ? 'mob' : 'dist'), // 运行时生成的生产环境构建文件的目录(默认''dist''，构建之前会被清除)
   assetsDir: 'public', // 放置生成的静态资源(s、css、img、fonts)的(相对于 outputDir 的)目录(默认'')
   runtimeCompiler: true, // 运行时版本是否需要编译\
   productionSourceMap: false, // 是否在构建生产包时生成 sourceMap 文件，false将提高构建速度
@@ -12,8 +12,8 @@ module.exports = {
     // pages 里配置的路径和文件名在你的文档目录必须存在 否则启动服务会报错
     index: {
       // 除了 entry 之外都是可选的
-      // entry: 'mobile/main.js', // 手机端 // page 的入口,每个“page”应该有一个对应的 JavaScript 入口文件
-      entry: 'pc/main.js', // pc端 // page 的入口,每个“page”应该有一个对应的 JavaScript 入口文件
+      entry: 'mobile/main.js', // 手机端 // page 的入口,每个“page”应该有一个对应的 JavaScript 入口文件
+      // entry: process.env.NODE_ENV === 'moblie' ? 'mobile/main.js' : 'pc/main.js', // pc端 // page 的入口,每个“page”应该有一个对应的 JavaScript 入口文件
       template: 'public/index.html', // 模板来源
       filename: 'index.html', // 在 dist/index.html 的输出
       title: 'Index Page', // 当使用 title 选项时,在 template 中使用：<title><%= htmlWebpackPlugin.options.title %></title>
@@ -24,7 +24,7 @@ module.exports = {
     // sourceMap: true, //  方便调试样式。  会影响编译速度，平时要关闭
     loaderOptions: {
       sass: {
-        prependData: '@import \'./pc/style/theme.css\';'
+        prependData: '@import \'./mobile/style/theme.scss\';'
       }
     }
   },
