1f63f37c1da7f9372bb515fcd34b9ab57991432b
mark.duo <mark.wang@shijigroup.com>
2020-01-20 11:14:43 +0800

    提交

---
+++ mobile/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Login />
+  </div>
+</template>
+
+<script>
+import Login from './views/login/login'
+// import Demo from './views/demo/demo'
+
+export default {
+  name: 'app',
+  components: {
+    Login
+    // Demo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 
 module.exports = {
+  // 部署应用包时的基本 URL,从 Vue CLI 3.3 起已弃用baseUrl
+  publicPath: '/',
   outputDir: path.resolve(__dirname, process.env.NODE_ENV === 'development' ? 'alpha' : 'dist'), // 运行时生成的生产环境构建文件的目录(默认''dist''，构建之前会被清除)
   assetsDir: 'public', // 放置生成的静态资源(s、css、img、fonts)的(相对于 outputDir 的)目录(默认'')
   runtimeCompiler: true, // 运行时版本是否需要编译\
@@ -10,7 +12,8 @@ module.exports = {
     // pages 里配置的路径和文件名在你的文档目录必须存在 否则启动服务会报错
     index: {
       // 除了 entry 之外都是可选的
-      entry: 'src/main.js', // page 的入口,每个“page”应该有一个对应的 JavaScript 入口文件
+      // entry: 'mobile/main.js', // 手机端 // page 的入口,每个“page”应该有一个对应的 JavaScript 入口文件
+      entry: 'pc/main.js', // pc端 // page 的入口,每个“page”应该有一个对应的 JavaScript 入口文件
       template: 'public/index.html', // 模板来源
       filename: 'index.html', // 在 dist/index.html 的输出
       title: 'Index Page', // 当使用 title 选项时,在 template 中使用：<title><%= htmlWebpackPlugin.options.title %></title>
@@ -21,7 +24,7 @@ module.exports = {
     // sourceMap: true, //  方便调试样式。  会影响编译速度，平时要关闭
     loaderOptions: {
       sass: {
-        prependData: '@import \'~@/style/theme.scss\';'
+        prependData: '@import \'./pc/style/theme.css\';'
       }
     }
   },
@@ -36,6 +39,7 @@ module.exports = {
     proxy: null, // 设置代理
     before: app => {}
   },
+  lintOnSave: false, // 关闭eslink
   // PWA 插件相关配置
   pwa: {},
   // 第三方插件配置
